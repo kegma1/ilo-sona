16e88ca63d4432c0db01b0048e5942675f130ea9
kegma1 <kennetam02@gmai.com>
2023-01-25 09:28:58 +0100

    home page

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+const textTp = document.getElementById("write-tp")
+const output = document.getElementById("output")
+
+function textareaUpdate() {
+    output.textContent = textTp.value
+}
+
+function formatTP(str) {
+    let inCartouche = false
+    for(let c in str) {
+            
+    }
+}
